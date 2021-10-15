f8d41fa0c3c8f2ee315eb317a41932714d05244b
Mathieu Chauvin <mathieu.chauvin.pro@gmail.com>
2021-10-15 12:37:50 +0200

    initialize and updating bets methods done

--- betly_contracts/programs/betly_contracts/src/lib.rs
+++ betly_contracts/programs/betly_contracts/src/lib.rs
@@ -3,39 +3,42 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-mod basic_1 {
+mod bet_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> ProgramResult {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+    pub fn create_bet(ctx: Context<CreateBet>, bet: u64, team:u8) -> ProgramResult {
+        let bet_account = &mut ctx.accounts.bet_account;
+        bet_account.bet = bet;
+        bet_account.team = team;
         Ok(())
     }
 
-    pub fn update(ctx: Context<Update>, data: u64) -> ProgramResult {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+    pub fn update_bet(ctx: Context<UpdateBet>, bet: u64, team:u8) -> ProgramResult {
+        let bet_account = &mut ctx.accounts.bet_account;
+        bet_account.bet = bet;
+        bet_account.team = team;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
-    pub my_account: Account<'info, MyAccount>,
+pub struct CreateBet<'info> {
+    #[account(init, payer = user, space = 8 + 8 + 3)]
+    pub bet_account: Account<'info, BetAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
+pub struct UpdateBet<'info> {
     #[account(mut)]
-    pub my_account: Account<'info, MyAccount>,
+    pub bet_account: Account<'info, BetAccount>,
 }
 
 #[account]
-pub struct MyAccount {
-    pub data: u64,
+pub struct BetAccount {
+    pub bet: u64,
+    pub team: u8
 }
 
--- betly_contracts/tests/betly_contracts.js
+++ betly_contracts/tests/betly_contracts.js
@@ -1,16 +1,67 @@
+const assert = require("assert")
 const anchor = require("@project-serum/anchor");
+const {SystemProgram} = anchor.web3;
 
-describe("basic-0", () => {
+describe("bet-program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.local());
+  const provider = anchor.Provider.local()
+  anchor.setProvider(provider);
 
   it("Uses the workspace to invoke the initialize instruction", async () => {
     // #region code
     // Read the deployed program from the workspace.
-    const program = anchor.workspace.Basic0;
+    const program = anchor.workspace.BetProgram;
+
+    const betAccount = anchor.web3.Keypair.generate();
+    const betAccount2 = anchor.web3.Keypair.generate();
 
     // Execute the RPC.
-    await program.rpc.initialize();
+    await program.rpc.createBet(new anchor.BN(1234), 3, {
+      accounts:{
+        betAccount: betAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [betAccount],
+    });
     // #endregion code
+
+    // Execute the RPC.
+    await program.rpc.createBet(new anchor.BN(12345), 4, {
+      accounts:{
+        betAccount: betAccount2.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [betAccount2],
+    });
+    // #endregion code
+
+    //const account = await program.account.betAccount.fetch(betAccount.publicKey);
+    const account= await program.account.betAccount.fetch(betAccount2.publicKey);
+
+    assert.ok(account.bet.eq(new anchor.BN(12345)));
+
+    _betAccount = betAccount;
+    _betAccount2 = betAccount2;
+  });
+
+  it("update the data", async() => {
+    const betAccount = _betAccount;
+    const betAccount2 = _betAccount2;
+    console.log(betAccount2.publicKey);
+      const program = anchor.workspace.BetProgram;
+
+      await program.rpc.updateBet(new anchor.BN(4321), 9, {
+        accounts: {
+          betAccount: _betAccount.publicKey,
+        },
+      });
+
+      const account = await program.account.betAccount.fetch(betAccount.publicKey);
+      const account2 = await program.account.betAccount.fetch(betAccount2.publicKey);
+
+      assert.ok(account.bet.eq(new anchor.BN(4321)));
+      assert.ok(account2.team === 4);
   });
 });
